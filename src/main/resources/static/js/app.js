e135c6afedafa393b2fb2af6ab9d4c1ab0191b03
carol.meneses <carol.meneses@mail.escuelaing.edu.co>
2017-09-25 17:07:48 -0500

    parte lógica adelanto2

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -5,36 +5,41 @@
  */
 
 //CREAR MODULO
-var Module = (function () {
+var module = (function () {
 // code
-var nombre;
-        var nom_pun = [];
-        
-        return{
+    var nombre;
+    var nom_pun;
+
+    return{
         setBlueprintsByName: function (authname) {
-        nombre = authname;
+            nombre = authname;
         },
-                setActualizar:function(authname){
-                apimock.getBlueprintsByAuthor(authname, function(lista){
+        setActualizar: function (autor) {
+            module.setBlueprintsByName(autor);
+            apimock.getBlueprintsByAuthor(nombre, function (lista) {
                 nom_pun = lista;
-                        // listado de los planos, y le aplique una función 'map' convertir json
-                        var lista1 = nom_pun.map(function(blue){
-                        return{"nombre":blue.nombre, "tamano":blue.points.length}; }
-                        );
-                        //otro 'map', que tome cada uno de estos elementos, y a través de jQuery agregue un elemento <tr> (con los respectvos <td>)
-                        var lista2 = lista1.map(function(valor){
-                        return "<tr><td>" + valor.nombre + "</td>\n\<td>" + valor.tamano + "</td></tr>";
-                        });
-                        // aplique un 'reduce' que calcule el número de puntos. Con este valor, use jQuery para actualizar el campo correspondiente
-                        var reduce = lista1.reduce(function(suma, value){
-                        return suma.points.length + value.points.length;
-                        }
-                        );
+                // listado de los planos, y le aplique una función 'map' convertir json
+                var lista1 = nom_pun.map(function (blue) {
+                    return{"name": blue.name, "tamano": blue.points.length};
                 }
                 );
-             }
-         };
-    })();
+                //otro 'map', que tome cada uno de estos elementos, y a través de jQuery agregue un elemento <tr> (con los respectvos <td>)
+                var lista2 = lista1.map(function (valor) {
+                    return "<tr><td>" + valor.name + "</td>\n\<td>" + valor.tamano + "</td></tr>";
+                });
+                // aplique un 'reduce' que calcule el número de puntos. Con este valor, use jQuery para actualizar el campo correspondiente
+                var adicion = lista1.reduce(function (valor, value) {
+                    
+                    return valor+value.tamano;
+                }
+                );
+                $("table").append(lista1);
+                $("Total de puntos").text(+ adicion);
+            }
+            );
+        }
+    };
+})();
 
 
 
